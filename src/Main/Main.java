ee359cfa91ea3d2b5f69026f8d491ee6e76e7457
pablosnc <jnpablosnc@tutanota.com>
2021-06-26 02:40:25 -0600

    Se crea el repositorio

---
+++ src/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+import Operaciones.Operaciones;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un ángulo en grados: ");
+        double angulo_en_grados = sc.nextDouble();
+
+    }
+}
