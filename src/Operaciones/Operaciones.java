63a0ad6104b00540e533c988d844d4558b0b18e7
LudwinRamosBriceno <85603585+LudwinRamosBriceno@users.noreply.github.com>
2021-06-26 17:05:09 -0600

    Se agregan las funciones trigonométricas
    Se definen las funciones que harán las operaciones sobre el ángulo dado con la respectiva función trigonométrica que el usuario escoja.

--- src/Operaciones/Operaciones.java
+++ src/Operaciones/Operaciones.java
@@ -1,4 +1,51 @@
 package Operaciones;
 
 public class Operaciones {
-}
+  
+  double resultado = 0;
+  
+  public double cos (double angulo_en_grados){
+      double angulo_en_radianes = Math.toRadians(angulo_en_grados);
+      resultado = Math.cos(angulo_en_radianes);
+      return resultado;
+  }
+  public double sin (double angulo_en_grados){
+      double angulo_en_radianes = Math.toRadians(angulo_en_grados);
+      resultado = Math.sin(angulo_en_radianes);
+      return resultado;
+  }
+  public double tan (double angulo_en_grados){
+      double angulo_en_radianes = Math.toRadians(angulo_en_grados);
+      resultado = Math.tan(angulo_en_radianes);
+      return resultado;
+  }
+  public double arccos (double angulo_en_grados){
+      double angulo_en_radianes = Math.toRadians(angulo_en_grados);
+      resultado = Math.acos(angulo_en_radianes);
+      return resultado;
+  }
+  public double arcsin (double angulo_en_grados){
+      double angulo_en_radianes = Math.toRadians(angulo_en_grados);
+      resultado = Math.asin(angulo_en_radianes);
+      return resultado;
+  }
+  public double arctan (double angulo_en_grados){
+      double angulo_en_radianes = Math.toRadians(angulo_en_grados);
+      resultado = Math.atan(angulo_en_radianes);
+      return resultado;
+  }
+  public double cosh (double angulo_en_grados){
+      double angulo_en_radianes = Math.toRadians(angulo_en_grados);
+      resultado = Math.cosh(angulo_en_radianes);
+      return resultado;
+  }
+  public double sinh (double angulo_en_grados){
+      double angulo_en_radianes = Math.toRadians(angulo_en_grados);
+      resultado = Math.sinh(angulo_en_radianes);
+      return resultado;
+  }
+  public double tanh (double angulo_en_grados){
+      double angulo_en_radianes = Math.toRadians(angulo_en_grados);
+      resultado = Math.tanh(angulo_en_radianes);
+      return resultado;
+  }
