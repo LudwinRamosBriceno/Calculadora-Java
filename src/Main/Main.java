db5b19b6d6e6a51d670a84ac95f0c3882ecf056a
LudwinRamosBriceno <85603585+LudwinRamosBriceno@users.noreply.github.com>
2021-06-27 22:17:11 -0600

    Se introduce un método switch
    Se agrega el método switch para realizar casos que corresponderán a las opciones que el usuario escogerá, para luego llamar a la función correspondiente y realizar la operación respectiva.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -13,6 +13,37 @@ public class Main {
         Operaciones op = new Operaciones();
         op.menu();
         int option = sc.nextInt();
-
+        switch (option){
+            case 1:
+                System.out.println(op.cos(angulo_en_grados));
+                break;
+            case 2:
+                System.out.println(op.sin(angulo_en_grados));
+                break;
+            case 3:
+                System.out.println(op.tan(angulo_en_grados));
+                break;
+            case 4:
+                System.out.println(op.arccos(angulo_en_grados));
+                break;
+            case 5:
+                System.out.println(op.arcsin(angulo_en_grados));
+                break;
+            case 6:
+                System.out.println(op.arctan(angulo_en_grados));
+                break;
+            case 7:
+                System.out.println(op.cosh(angulo_en_grados));
+                break;
+            case 8:
+                System.out.println(op.sinh(angulo_en_grados));
+                break;
+            case 9:
+                System.out.println(op.tanh(angulo_en_grados));
+                break;
+            default:
+                System.out.println("Error. Número fuera del rango establecido.");
+                break;
+        }
     }
 }
